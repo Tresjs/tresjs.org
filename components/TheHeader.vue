a62f0ff99fdbeb6e06ffd62f5ccd17767176d2e2
alvarosabu <alvaro.saburido@gmail.com>
2025-06-14 23:32:42 +0200

    refactor: replace useDark with useColorMode for dark mode handling; update styles for better dark mode support in various components

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -31,7 +31,7 @@ const items = computed<NavigationMenuItem[]>(() => [
     <template #title>
       <div class="flex items-center">
         <img src="/logo.svg" alt="Tres.js" class="h-3 mr-4">
-        <a class="font-bold text-dark font-sans" href="/" title="Home">Tres<span class="text-primary-300">JS</span></a>
+        <a class="font-bold text-dark font-sans text-sm" href="/" title="Home">Tres<span class="text-primary-300">JS</span></a>
       </div>
     </template>
     <UNavigationMenu :items="items" />
