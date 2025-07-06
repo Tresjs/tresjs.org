71ba40b441788f1a7ae2385cfa41d68bb84bdbde
alvarosabu <alvaro.saburido@gmail.com>
2025-07-06 13:02:10 +0200

    fix: update NuxtImg component in Card.vue to include sizes and format attributes for better image optimization

--- components/Showcase/Card.vue
+++ components/Showcase/Card.vue
@@ -8,7 +8,7 @@ defineProps<{
 <template>
   <div class="bg-default border-x sm:max-lg:nth-[2n+1]:border-l-0 lg:nth-[3n+1]:border-l-0 lg:nth-[3n]:border-r-0 border-solid border-gray-100 dark:border-default">
     <div class="p-4 relative py-2 px-4 before:absolute before:top-0 before:h-px before:w-[200vw] before:border-t before:border-solid before:border-gray-100 dark:before:border-default before:-left-[100vw] ">
-      <NuxtImg :src="item.thumbnail" :alt="item.title" class="w-full aspect-video object-cover h-auto rounded-lg" />
+      <NuxtImg :src="item.thumbnail" sizes="100vw sm:50vw md:372px" :alt="item.title" format="webp" class="w-full aspect-video object-cover h-auto rounded-lg" />
     </div>
     <div class="flex justify-between items-center relative py-2 px-4 before:absolute before:top-0 before:h-px before:w-[200vw] before:border-t before:border-solid before:border-gray-100 dark:before:border-default before:-left-[100vw]  after:w-[200vw] after:absolute after:bottom-0 after:h-px after:border-t after:border-solid after:border-gray-100 dark:after:border-default after:-left-[100vw]">
       <div>
